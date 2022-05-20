b9eb6d15c0648a91a4a736e547bfa06dee3a4d13
joaomattedi <joaommattedi@gmail.com>
2022-05-20 16:33:54 -0300

    requisito 5

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,32 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const totalEntry = {
+    child: entrants.filter(({ age }) => age < 18).length,
+    adult: entrants.filter(({ age }) => age >= 18 && age < 50).length,
+    senior: entrants.filter(({ age }) => age >= 50).length,
+  };
+
+  return totalEntry;
 }
 
+const { adult: adultPrice, child: childPrice, senior: seniorPrice } = data.prices;
+
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { adult, child, senior } = countEntrants(entrants);
+  return (adult * adultPrice) + (child * childPrice) + (senior * seniorPrice);
 }
 
 module.exports = { calculateEntry, countEntrants };
-// first commit
+
+// const entrants = [
+//   { name: name.findName(), age: 5 },
+//   { name: name.findName(), age: 5 },
+//   { name: name.findName(), age: 5 },
+//   { name: name.findName(), age: 18 },
+//   { name: name.findName(), age: 18 },
+//   { name: name.findName(), age: 50 },
+// ];
