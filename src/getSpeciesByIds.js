93342ebcc9def9f5cf7f1f4d72f8a3f33f56fc5d
joaomattedi <joaommattedi@gmail.com>
2022-05-20 14:52:21 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (!ids) {
+    return [];
+  }
+
+  return data.species.filter(({ id }) => ids.includes(id));
+
 }
 
 module.exports = getSpeciesByIds;
