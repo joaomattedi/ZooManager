424a42acab590a34e510d36255a141c4cd22c043
joaomattedi <joaommattedi@gmail.com>
2022-05-20 15:26:15 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees
+    .filter(({ managers }) => (managers.includes(managerId)))
+    .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
