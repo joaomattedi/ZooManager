917bfb715e86f272574aa3c6f1fae7f0add7eaa4
joaomattedi <joaommattedi@gmail.com>
2022-05-20 17:28:09 -0300

    requisito 5 e 8

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species
+      .reduce((acc, { name, residents }) => ({ ...acc, [name]: residents.length }), {});
+  }
+  const { specie, sex } = animal;
+
+  if (specie && sex) {
+    return data.species
+      .find(({ name }) => name === specie).residents
+      .filter(({ sex: sexAnimal }) => sexAnimal === sex).length;
+  }
+
+  if (specie) {
+    return data.species.find(({ name }) => name === specie).residents.length;
+  }
+
 }
 
 module.exports = countAnimals;
