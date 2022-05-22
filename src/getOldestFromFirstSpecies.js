4124683f8bc7a5f1b8d1c8e859e7cdb05c19a485
joaomattedi <joaommattedi@gmail.com>
2022-05-22 11:33:20 -0300

    requisito 10 lindo

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const getOldestResident = (animalId) => data.species
+  .find(({ id }) => id === animalId).residents
+  .reduce((acc, cur) => {
+    const { name, sex, age } = cur;
+    if (acc.length === 0) {
+      return [name, sex, age];
+    }
+    if (acc[2] > age) {
+      return acc;
+    }
+    return [name, sex, age];
+  }, []);
+
+function getOldestFromFirstSpecies(employeeId) {
+  const firstAnimalOfEmployee = data.employees
+    .find(({ id }) => id === employeeId).responsibleFor[0];
+  return getOldestResident(firstAnimalOfEmployee);
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+// console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
