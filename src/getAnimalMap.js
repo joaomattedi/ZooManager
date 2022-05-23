d88d9b564d238f93e42e81c9c334f12fae9e591b
joaomattedi <joaommattedi@gmail.com>
2022-05-23 15:18:11 -0300

    TERMINEI

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
+const locations = ['NE', 'NW', 'SE', 'SW'];
+
+const getAnimals = (location) => data.species.filter(({ location: animalLocation }) =>
+  location === animalLocation).map(({ name }) => name);
+
+const getAnimalsName = (animal, sorted, sex) => {
+  if (sex && sorted) {
+    return ({ [animal]: data.species.find(({ name }) =>
+      name === animal).residents.filter(({ sex: animalSex }) =>
+      sex === animalSex).map(({ name }) => name).sort() });
+  }
+  if (sorted) {
+    return ({ [animal]: data.species.find(({ name }) =>
+      name === animal).residents.map(({ name }) => name).sort() });
+  }
+  if (sex) {
+    return ({ [animal]: data.species.find(({ name }) =>
+      name === animal).residents.filter(({ sex: animalSex }) =>
+      sex === animalSex).map(({ name }) => name) });
+  }
+  return ({ [animal]: data.species.find(({ name }) =>
+    name === animal).residents.map(({ name }) => name) });
+};
+
+const getAnimalsByLocation = (sorted, sex) => locations.reduce((acc, cur) =>
+  ({ ...acc,
+    [cur]: getAnimals(cur).map((specieName) => getAnimalsName(specieName, sorted, sex)) }), {});
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) {
+    return locations.reduce((acc, cur) =>
+      ({ ...acc, [cur]: getAnimals(cur) }), {});
+  }
+  const { includeNames, sorted, sex } = options;
+  if (!includeNames) {
+    return locations.reduce((acc, cur) =>
+      ({ ...acc, [cur]: getAnimals(cur) }), {});
+  }
+  return getAnimalsByLocation(sorted, sex);
 }
 
 module.exports = getAnimalMap;
+
+console.log(getAnimalMap());
